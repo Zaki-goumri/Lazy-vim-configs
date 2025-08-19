83c753767ce18b85127013772d3d71a5f9aa6982
zaki <z.goumri@esi-sba.dz>
2025-08-19 15:47:58 +0100

    feat(debug):add debug keymap

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -19,6 +19,28 @@ return {
       dap.listeners.before.event_exited["dapui_config"] = function()
         dapui.close()
       end
+
+      vim.keymap.set("n", "<F5>", function()
+        dap.continue()
+      end)
+      vim.keymap.set("n", "<F10>", function()
+        dap.step_over()
+      end)
+      vim.keymap.set("n", "<F11>", function()
+        dap.step_into()
+      end)
+      vim.keymap.set("n", "<F12>", function()
+        dap.step_out()
+      end)
+      vim.keymap.set("n", "<leader>b", function()
+        dap.toggle_breakpoint()
+      end)
+      vim.keymap.set("n", "<leader>B", function()
+        dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
+      end)
+      vim.keymap.set("n", "<leader>dr", function()
+        dap.repl.open()
+      end)
     end,
   },
 }
