ea499e026b81380e8ba1ef5fbc5638bbcff023f7
zaki <z.goumri@esi-sba.dz>
2025-09-03 11:17:29 +0100

    some updates

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -20,27 +20,50 @@ return {
         dapui.close()
       end
 
+      -- Keymaps (custom prefix <leader>m)
       vim.keymap.set("n", "<F5>", function()
         dap.continue()
-      end)
+      end, { desc = "DAP Continue" })
       vim.keymap.set("n", "<F10>", function()
         dap.step_over()
-      end)
+      end, { desc = "DAP Step Over" })
       vim.keymap.set("n", "<F11>", function()
         dap.step_into()
-      end)
+      end, { desc = "DAP Step Into" })
       vim.keymap.set("n", "<F12>", function()
         dap.step_out()
-      end)
-      vim.keymap.set("n", "<leader>b", function()
+      end, { desc = "DAP Step Out" })
+
+      vim.keymap.set("n", "<leader>mb", function()
         dap.toggle_breakpoint()
-      end)
-      vim.keymap.set("n", "<leader>B", function()
+      end, { desc = "DAP Toggle Breakpoint" })
+      vim.keymap.set("n", "<leader>mB", function()
         dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
-      end)
-      vim.keymap.set("n", "<leader>dr", function()
+      end, { desc = "DAP Conditional Breakpoint" })
+      vim.keymap.set("n", "<leader>mr", function()
         dap.repl.open()
-      end)
+      end, { desc = "DAP REPL" })
+
+      -- Node.js / TypeScript adapter (requires vscode-js-debug installed)
+      dap.adapters.node2 = {
+        type = "executable",
+        command = "node",
+        args = { os.getenv("HOME") .. "/.config/nvim/vscode-js-debug/out/src/debugServerMain.js" },
+      }
+
+      dap.configurations.typescript = {
+        {
+          type = "node2",
+          request = "launch",
+          name = "Launch file",
+          program = "${file}",
+          cwd = vim.fn.getcwd(),
+          sourceMaps = true,
+          protocol = "inspector",
+        },
+      }
+
+      dap.configurations.javascript = dap.configurations.typescript
     end,
   },
 }
